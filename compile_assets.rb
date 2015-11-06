bdbc8760757419d1196ca64e156b1bf698e8d275
Nigel Baillie <metreckk@gmail.com>
2015-11-06 01:39:44 -0500

    very rudamentary asset packaging!

---
+++ compile_assets.rb
@@ -0,0 +1,26 @@
+assets = File.new("build/crattlecrute.assets", 'wb')
+
+header = File.new("src/assets.h", 'w')
+header.write("// Generated #{Time.now}\n\n")
+
+current_offset = 0
+
+Dir.glob("./assets/**/*").each do |file|
+  next if File.directory? file
+
+  bytes = IO.binread(file)
+  assets.write(bytes)
+
+  puts file.gsub! /^\.\/assets\//, ''
+  file_ident = file.gsub(/[\.\s\?!\/\\-]/, '_').upcase
+
+  header.write(
+    "const int #{file_ident}_OFFSET = #{current_offset};\n"\
+    "const int #{file_ident}_SIZE = #{bytes.size};\n"
+  )
+
+  current_offset += bytes.size
+end
+
+assets.close
+header.close
--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include "SDL.h"
+#include "assets.h"
 
 int main(int argc, char** argv) {
     SDL_Window* window;
@@ -14,8 +17,24 @@ int main(int argc, char** argv) {
         SDL_WINDOW_OPENGL
     );
 
-    SDL_Delay(1000);
+    FILE* assets_file = fopen("crattlecrute.assets", "rb");
+    unsigned char buf[1000];
+    fseek(assets_file, TERRAIN_DIRT1_PNG_OFFSET, SEEK_SET);
+    if (!assets_file) printf("INSANE WHAT HAPPEN\n");
+    size_t size_read = fread(buf, 1, TERRAIN_DIRT1_PNG_SIZE, assets_file);
+    if (size_read != TERRAIN_DIRT1_PNG_SIZE)
+        printf("WHAT THE FUCK\n");
 
+    int x = 0, y = 0, comp = 0;
+    stbi_uc* image = stbi_load_from_memory(buf, TERRAIN_DIRT1_PNG_SIZE, &x, &y, &comp, 4);
+    if (image == NULL)
+        printf("ERROR: %s\n", stbi_failure_reason());
+    else
+        printf("OK we have %ix%i image of %i pixel components.\n", x, y, comp);
+
+    SDL_Delay(10000);
+
+    stbi_image_free(image);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
