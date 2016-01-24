63e07ecf071117b9d80f356f53b2f03f10c7c0b1
Nigel Baillie <metreckk@gmail.com>
2016-01-23 23:26:39 -0500

    try tampering with c4android config

--- androidbuild.sh
+++ androidbuild.sh
@@ -1,1 +1,1 @@
-gcc src/*.c -ISTB -ISDL2 -IEGL -IGLESv1_CM -IGLESV2 -Iandroid -I(c4android:GCCROOT)(c4android:PREFIX)/include/SDL2 -WI,--no-undefined -shared -g -O0 -D_DEBUG
+ld -r -b binary -o build/crattlecrute.assets.o build/crattlecrute.assets
