fa1e5e6dfac5558edf3039bf7eb381369e51b01c
Nigel Baillie <metreckk@gmail.com>
2016-03-04 12:20:14 -0500

    mruby now on linux

--- linuxbuild.rb
+++ linuxbuild.rb
@@ -13,7 +13,7 @@ if ARGV.include? 'noembed'
     system "gcc src/*.c -std=c99 -ISTB -ISDL/include -lSDL2 -lm -o build/crattlecrute #{debug_flags}"
 else
     system "ld -r -b binary -o build/crattlecrute.assets.o build/crattlecrute.assets"
-    system "gcc src/*.c -std=c99 build/crattlecrute.assets.o -ISTB -ISDL/include -lSDL2 -lm -DEMBEDDED_ASSETS -o build/crattlecrute #{debug_flags}"
+    system "gcc src/*.c -std=c99 build/crattlecrute.assets.o -ISTB -ISDL/include -IMRuby/include -LMRuby/build/host/lib -lSDL2 -lm -lmruby -DEMBEDDED_ASSETS -o build/crattlecrute #{debug_flags}"
     File.unlink 'build/crattlecrute.assets', 'build/crattlecrute.assets.o'
 end
 puts "DONE"
