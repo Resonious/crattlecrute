f31ecba4f12d35f300ffb45cbaedff6400df940c
Nigel Baillie <metreckk@gmail.com>
2016-03-19 17:04:17 +0000

    use aligned malloc in main.c

--- src/main.c
+++ src/main.c
@@ -90,6 +90,7 @@ mrb_value rb_exit(mrb_state* mrb, mrb_value self) {
 
 int main(int argc, char** argv) {
     BENCH_START(total_initialization);
+    SDL_assert(sizeof(byte) == 1);
     srand((unsigned int)time(0));
     ticks_per_second = SDL_GetPerformanceFrequency();
     _MM_SET_ROUNDING_MODE(_MM_ROUND_DOWN);
@@ -111,11 +112,7 @@ int main(int argc, char** argv) {
 #endif
 
     game.current_scene = &SCENES[SCENE_WORLD];
-#ifdef X86
-    game.current_scene_data = _aligned_malloc(SCENE_DATA_SIZE, 16);
-#else
-    game.current_scene_data = malloc(SCENE_DATA_SIZE);
-#endif
+    game.current_scene_data = aligned_malloc(SCENE_DATA_SIZE);
     game.camera.simd = _mm_set1_ps(0.0f);
     game.camera_target.simd = _mm_set1_ps(0.0f);
     game.follow_cam_y = false;
@@ -440,6 +437,7 @@ no_renderer:
         SDL_DetachThread(ruby_repl);
 #endif
     game.current_scene->cleanup(game.current_scene_data, &game);
+    aligned_free(game.current_scene_data);
     SDL_PauseAudio(true);
     SDL_DestroyWindow(game.window);
 
